6ce7be74e650f04429a2f6cfb697aecddb2741fa
Loriano Storchi <loriano@storchi.org>
2019-04-17 15:31:26 +0200

    changed color and shape via style

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,14 +7,44 @@
 #include <QWidget>
 #include <QColor>
 
+#include <iostream>
+
 namespace
 {
     const QString getColor (int value)
     {
-      if (value <= 50)
-        return QString("green");
+      QString basic = QString("QProgressBar {"
+         "border: 1px solid black;"
+         "text-align: top;"
+         "padding: 1px;"
+         "border-bottom-right-radius: 7px;"
+         "border-bottom-left-radius: 7px;"
+         "width: 50px;}");
+      QString green = QString("QProgressBar::chunk {"
+          "background-color: #7CFC00;" 
+          "border-bottom-right-radius: 7px;" 
+          "border-bottom-left-radius: 7px;" 
+          "border: 1px solid black;" 
+          "}");
+      QString yellow = QString("QProgressBar::chunk {"
+          "background-color: #FFFF00;" 
+          "border-bottom-right-radius: 7px;" 
+          "border-bottom-left-radius: 7px;" 
+          "border: 1px solid black;" 
+          "}");
+      QString red = QString("QProgressBar::chunk {"
+          "background-color: #FF0000;" 
+          "border-bottom-right-radius: 7px;" 
+          "border-bottom-left-radius: 7px;" 
+          "border: 1px solid black;" 
+          "}");
+
+      if (value <= 30)
+        return basic + green;
+      else if (value > 30 && value < 60)
+        return basic + yellow;
       else
-        return QString("red");
+        return basic + red;
     };
 } 
 
@@ -28,19 +58,21 @@ MainWindow::MainWindow(QWidget *parent) :
     QWidget * centralwdg = new QWidget(this);
     QGridLayout *vlay = new QGridLayout(centralwdg);
     QPushButton *btn1 = new QPushButton("Decrease");
-    vlay->addWidget(btn1, 0,0,1,1);
+    vlay->addWidget(btn1,0,0,1,1);
     QPushButton *btn2 = new QPushButton("Increase");
-    vlay->addWidget(btn2, 0,2,1,1);
+    vlay->addWidget(btn2,0,2,1,1);
 
     _bar = new QProgressBar();
+    _bar->setOrientation(Qt::Vertical);
     _bar->setMinimum(0);
     _bar->setMaximum(100);
     _bar->setValue(0);
+    _bar->setTextVisible(true);
 
     connect(btn1, SIGNAL(clicked()), this, SLOT(decrease()));
     connect(btn2, SIGNAL(clicked()), this, SLOT(increase()));
 
-    vlay->addWidget(_bar, 1,1,10,1);
+    vlay->addWidget(_bar,1,1,10,1);
 
     centralwdg->setLayout(vlay);
 
@@ -56,9 +88,7 @@ void MainWindow::increase()
 {
     int value = _bar->value();
     value += 1;
-    QPalette p = _bar->palette();
-    p.setColor(QPalette::Highlight, QColor(getColor(value)));
-    _bar->setPalette(p);
+    setStyleSheet (getColor(value));
     _bar->setValue(value);
 }
 
@@ -66,9 +96,6 @@ void MainWindow::decrease()
 {
     int value = _bar->value();
     value -= 1;
-    QPalette p = _bar->palette();
-    p.setColor(QPalette::Highlight, QColor(getColor(value)));
-    _bar->setPalette(p);
+    setStyleSheet (getColor(value));
     _bar->setValue(value);
-
 }
